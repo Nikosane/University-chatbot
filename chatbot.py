4c5874fc7c3132a2f1e677e67b621dbddb6be2d6
Nikosane <niteshkotian3@gmai.com>
2023-10-20 23:51:39 +0530

    setting up the backend

--- chatbot.py
+++ chatbot.py
@@ -16,38 +16,36 @@ data = torch.load(FILE)
 
 input_size = data["input_size"]
 hidden_size = data["hidden_size"]
-output_size = data ["output_size"]
+output_size = data["output_size"]
 all_words = data["all_words_size"]
-tags = data ["tags"]
+tags = data["tags"]
 model_state = data["model_state"]
 
-
-model = NeuralNet(input_size, hidden_size,output_size).to(device)
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
 
 bot_name = "University ChatBot"
-print("Let's chat ! type 'quit' to exit ")
-while True :
-    sentence = input("You : ")
-    if sentence =="quit":
-        break
 
-    sentence = tokenize(sentence)
-    x = bag_of_words(sentence ,all_words)
-    x = x.reshape(1 ,x.shape[0])
+
+def get_response(msg):
+    sentence = tokenize(msg)
+    x = bag_of_words(sentence, all_words)
+    x = x.reshape(1, x.shape[0])
     x = torch.from_numpy(x)
 
     output = model(x)
-    _,predicted = torch.max(output, dim=1)
+    _, predicted = torch.max(output, dim=1)
     tag = tags[predicted.item()]
 
-    probs = torch.softmax(output , dim=1)
+    probs = torch.softmax(output, dim=1)
     prob = probs[0][predicted.item()]
 
-    if prob.item() > 0.75 :
+    if prob.item() > 0.75:
         for intent in intents["intents"]:
             if tag == intent["tag"]:
-                print(f"{bot_name}:{random.choice(intent['responses'])}")
-    else:
-        print(f"{bot_name}: I do not understand...")
+                return random.choice(intent['responses'])
+    return "I don't understand..."
+
+
+
