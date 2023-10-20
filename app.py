5a6cae1806594ffa4e1393abea3b4898bb5b8d3e
Nikosane <niteshkotian3@gmai.com>
2023-10-21 00:09:19 +0530

    setting up the GUI

--- app.py
+++ app.py
@@ -11,18 +11,19 @@ FONT_BOLD = "Helvetica 14 bold"
 
 class ChatApplication:
 
-    def __int__(self):
+    def __init__(self):
         self.window = Tk()
         self._setup_main_window()
 
     def run(self):
-        self.window.main.loop()
+        self.window.mainloop()
 
     def _setup_main_window(self):
         self.window.title("Chat")
         self.window.resizable(width=False, height=False)
         self.window.configure(width=470, height=550, bg=BG_COLOR)
 
+
 if __name__ == "__main__":
-    app = ChatApplication
+    app = ChatApplication()
     app.run()
--- chatbot.py
+++ chatbot.py
@@ -17,7 +17,7 @@ data = torch.load(FILE)
 input_size = data["input_size"]
 hidden_size = data["hidden_size"]
 output_size = data["output_size"]
-all_words = data["all_words_size"]
+all_words = data["all_words"]
 tags = data["tags"]
 model_state = data["model_state"]
 
